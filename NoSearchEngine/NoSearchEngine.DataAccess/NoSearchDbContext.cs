0c3391a889e7c197a6b00b03dbb351d42c675fc3
Paul Michaels <pcmichaels@live.com>
2020-04-23 08:34:36 +0100

    Remove twitter keys into secrets file (and roll the keys).
    Add AspNetUser migrations.

--- NoSearchEngine/NoSearchEngine.DataAccess/NoSearchDbContext.cs
+++ NoSearchEngine/NoSearchEngine.DataAccess/NoSearchDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using NoSearchEngine.DataAccess.Entities;
@@ -17,5 +18,18 @@ namespace NoSearchEngine.DataAccess
         }
 
         public DbSet<ResourceEntity> ResourceEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<ApplicationUserEntity>().ToTable("User");            
+            builder.Entity<IdentityRole>().ToTable("Role");
+            builder.Entity<IdentityUserRole<string>>().ToTable("UserRole");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaim");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogin");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserToken");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaim");
+        }
     }
 }
